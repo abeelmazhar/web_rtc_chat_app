c385d8e5cf9d73fe81346acecde3b30290a1c934
abeelmazhar4@gmail.com <abeelmazhar>
2024-05-03 01:13:50 +0500

    socket created

--- index.js
+++ index.js
@@ -20,6 +20,32 @@ app.get('/',(req,res)=>
     res.send('chat server is running');
 });
 
+io.on('connection',(socket)=>
+{
+    socket.emit('me',socket.id);
+
+    socket.on('disconnect',()=>
+    {
+        socket.broadcast.emit('callEnded');
+    });
+    socket.on(
+        "callUser",
+        ({userToCall,signalData,from,name})=>
+        {
+            io.to(userToCall).emit("callUser",{signal:signalData,from,name});
+        }
+    )
+
+    socket.on(
+        "answerCall",
+        (data)=>
+        {
+            io.to(data.to).emit("callAccepted",data.signal);
+        }
+    )
+});
+
+
 server.listen(PORT,()=>
 {
     console.log(`server running on port ${PORT}`);
