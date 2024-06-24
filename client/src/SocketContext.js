3951ad7ecc473c5cd0bc7a1a61f74bc9af5f4365
abeelmazhar <abeelmazhar4@gmail.com>
2024-06-25 00:22:12 +0500

    updated

--- client/src/SocketContext.js
+++ client/src/SocketContext.js
@@ -4,7 +4,7 @@ import Peer from 'simple-peer'
 
 const SocketContext = createContext()
 const socket = io('http://localhost:3000')
-
+const a=2;
 const ContextProvider = ({children}) => {
 
     const [stream, setStream] = useState(null)
